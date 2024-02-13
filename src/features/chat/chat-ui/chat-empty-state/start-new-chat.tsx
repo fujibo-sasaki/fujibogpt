8deb5965bb3c5de5f07770b4a486616ba0ec1d53
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-13 14:16:35 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -18,16 +18,20 @@ export const StartNewChat: FC<Prop> = (props) => {
         </Typography>
         <div className="flex flex-col gap-2">
           <p className="">
-          富士紡GPTは、文章の要約や生成、WEB検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。
+          富士紡GPTは、文章の要約や生成、WEB検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。<br/>
           この度、DXの一環として、2024年3月からプレビュー版を社内利用限定で公開します。
           富士紡GPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております。
-          以下の注意事項を確認の上、富士紡GPTをご活用ください。
+          以下の注意事項をご確認の上、富士紡GPTをご活用ください。詳細はマニュアルをご参照ください。
           </p>
           <p>[注意事項]<br/>
           ・著作権侵害をしてしまう恐れがあるため、第三者の著作物を入力しない。固有名(既存著作物、作家名、作品の名称)を入力しない<br/>
           ・生成された回答結果に著作権侵害（既存の著作物との類似性・依拠性）がないか確認する<br/>
           ・回答結果の出典(根拠・裏付け)の確認や、回答結果の正確性を確認する<br/>
           <br/>
+          </p>
+          <p>[マニュアル]<br/>
+          <a href="https://http://groupeip1.intrafujibo.co.jp/keijiban/helppdf.nsf/MainTopicCopyCreationView/25EE7DC54E5E10D3492589250028B477?openDocument" target="_blunk">
+            操作マニュアル</a>
           </p>
           <p>下記の➕を押して会話を開始できます</p>
         </div>
