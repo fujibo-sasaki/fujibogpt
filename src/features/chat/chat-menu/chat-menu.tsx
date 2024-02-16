3f7846b0915e9fd5328e3f037b61b94584b06ec6
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-16 17:50:00 +0900

    Update chat-menu.tsx

--- src/features/chat/chat-menu/chat-menu.tsx
+++ src/features/chat/chat-menu/chat-menu.tsx
@@ -5,7 +5,7 @@ import { NewChat } from "./new-chat";
 
 export const ChatMenu = async () => {
   const items = await FindAllChatThreadForCurrentUser();
-  const img_style = {objectFit: contain};
+  const img_style = {};
   return (
     <Menu className=" p-2">
        <img src="/icon.jpg" style={img_style} />
