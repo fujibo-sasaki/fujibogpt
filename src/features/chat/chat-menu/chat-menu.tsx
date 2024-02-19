f836bfd276dc1d1d249e4fb2f293d5b98878a664
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-19 11:01:28 +0900

    Update chat-menu.tsx

--- src/features/chat/chat-menu/chat-menu.tsx
+++ src/features/chat/chat-menu/chat-menu.tsx
@@ -10,7 +10,7 @@ export const ChatMenu = async () => {
   
   return (
     <Menu className=" p-2">
-       <img src="/icon.jpg" className={styles.imgstyle} />
+       <img src="/icon.jpg" className="object-contain" />
       <MenuHeader className="justify-end">
         <NewChat />
       </MenuHeader>
