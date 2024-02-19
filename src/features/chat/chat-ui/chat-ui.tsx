cf8be5cc8b1b7989a452629994c0712b27f25a59
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-19 15:38:31 +0900

    Update chat-ui.tsx

--- src/features/chat/chat-ui/chat-ui.tsx
+++ src/features/chat/chat-ui/chat-ui.tsx
@@ -13,7 +13,7 @@ export const ChatUI: FC<Prop> = () => {
 
   return (
     <div className="h-full relative overflow-hidden flex-1 bg-card rounded-md shadow-md">
-      <img src="/icon.jpg" className="object-contain w-40 h-50 relative top-0 left-0" />
+      <img src="/icon.jpg" className="object-contain w-40 h-50 relative top-0 right-0" />
       {messages.length !== 0 ? (
         <ChatMessageContainer />
       ) : (
