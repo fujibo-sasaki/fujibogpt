d4a15bce9fb7f52d5f143e045faa053514c42be7
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-20 09:44:20 +0900

    Update page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <Card className="h-full items-center flex justify-center flex-1">
-    <img src="/icon.jpg" className="object-contain w-40 h-50 relative top-0 right-0" align="right"/>
+    <img src="/icon.jpg" className="object-contain w-40 h-50 relative top-0 right-0 justify-end" />
       <StartNewChat />
     </Card>
   );
