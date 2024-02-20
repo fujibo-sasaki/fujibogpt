21f0dee31bb942863560f05a3d01df94144a4949
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-20 09:23:28 +0900

    Update page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -6,6 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <Card className="h-full items-center flex justify-center flex-1">
+      test
       <StartNewChat />
     </Card>
   );
