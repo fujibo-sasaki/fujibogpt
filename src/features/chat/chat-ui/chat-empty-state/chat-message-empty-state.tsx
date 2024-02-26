4ae859977a4db549deeb0b1ef2a27464dea02781
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-26 11:04:13 +0900

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -22,13 +22,31 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
         <Typography variant="h4" className="text-primary">
           {AI_NAME}にようこそ！
         </Typography>
-          <p className="text-xs text-muted-foreground">
+           {/* <p className="text-xs text-muted-foreground">
             このChatGPT搭載のAIチャットボットは、2024年1月からプレビューで社内利用限定で公開されています。
             まだまだ未熟なAIですが、皆様のご協力により、AIの成長を目指しています。
             ご利用の際は、以下の項目を選択してください。
 
+          </p> */}
+          <p className="">
+          富士紡GPTは文章の要約や生成、WEB検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。<br/>
           </p>
-
+          <p>この度、DXの一環として、2024年3月からプレビュー版を社内利用限定で公開いたします。</p>
+          <p>以下の注意事項をご確認の上、富士紡GPTのご利用をお願いいたします。詳細はマニュアルをご参照下さい。<br/></p>
+          <p style={font_style}>※富士紡GPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p>
+          <br/>
+          <p>【注意事項】<br/>
+          ・第三者の著作物を入力しない。固有名詞(既存著作物、作家名、作品の名称)を入力しない<br/>
+          ・生成された回答結果に著作権侵害（既存の著作物との類似性・依拠性）がないか確認する<br/>
+          ・回答結果の出典(根拠・裏付け)の確認や、回答結果の正確性を確認する<br/>
+          ・業務以外のプライベートな利用は控える
+          </p>
+          <p>【マニュアル】<br/>
+          &nbsp;&nbsp;
+            <a href="http://groupeip1.intrafujibo.co.jp/keijiban/helppdf.nsf/MainTopicCopyCreationView/25EE7DC54E5E10D3492589250028B477?openDocument" target="_blank" style={a_style}>
+            操作マニュアル
+            </a>
+          </p>
 
         <div className="flex flex-col gap-2">
           <p className="text-sm text-muted-foreground">
