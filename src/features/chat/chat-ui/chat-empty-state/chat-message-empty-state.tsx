eda4a3655c98446750c4a14e7bb7a02963a64e9d
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-27 14:53:05 +0900

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -73,7 +73,7 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
           会話スタイルを選択してください。
           <Button
           asChild
-          className="rounded-full w-[10px] h-[10px] p-2 text-primary"
+          className="rounded-full w-[10px] h-[10px] p-2 text-primary text-black"
           variant={"outline"}
           >
             <Link target="_blank" href='https://prompt.quel.jp/index.php?imode=1&theme=%E3%83%93%E3%82%B8%E3%83%8D%E3%82%B9' title="便利な使い方" className="relative">
