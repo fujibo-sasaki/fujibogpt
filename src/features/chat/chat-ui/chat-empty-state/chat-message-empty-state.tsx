b8d6f83b80197fb55944e1737395f44c4ad6c8f5
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-27 17:18:32 +0900

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -54,7 +54,7 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
           をご参照下さい。
              <br/>
           </p>
-          <p style={font_style} className="text-sm">※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p><br/>
+          <p style={font_style} className="text-sm">※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております<br/></p>
           <p className="text-sm">【注意事項】<br/>
           ・第三者の著作物を入力しない。固有名詞(既存著作物、作家名、作品の名称)を入力しない<br/>
           ・生成された回答結果に著作権侵害（既存の著作物との類似性・依拠性）がないか確認する<br/>
@@ -62,18 +62,18 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
           ・業務以外のプライベートな利用は控える
           </p>
           <div className="flex">
-            <div className="flex-none w-7 h-14 gap-0"><HelpCircle /></div><div className="flex-none w-36 h-14">活用事例はこちら</div>
-            <div className="flex-none w-7 h-14 gap-0"><Info /></div><div className="flex-none w-64 h-14">会話スタイルについてはこちら</div>
+            <div className="flex-none w-7 h-14 gap-0"><HelpCircle /></div><div className="flex-none w-80 h-14">会話スタイル・利用機能についてはこちら</div>
+            <div className="flex-none w-7 h-14 gap-0"><Info /></div><div className="flex-none w-64 h-14">活用事例についてはこちら</div>
           </div>
         <div className="flex flex-col gap-2">
-          <p className="text-sm text-muted-foreground">
+          <br/><p className="text-sm text-muted-foreground">
           会話スタイルを選択してください。
           </p>
           <ChatStyleSelector disable={false} />
         </div>
         <div className="flex flex-col gap-2">
           <p className="text-sm text-muted-foreground">
-            AIがお手伝いする方法を選択してください。
+            利用機能を選択してください。
           </p>
           <ChatTypeSelector disable={false} />
         </div>
