de4d49d3d52cf12ee5ccce0f6de461cf4e086e8f
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-02-28 13:32:32 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -11,16 +11,16 @@ export const StartNewChat: FC<Prop> = (props) => {
   const p_style = {fontSize: "15px"};
   return (
     <div className="grid grid-cols-1 w-full items-center container mx-auto max-w-4xl justify-center h-full gap-9">
-      <Card className="gap-5 pr-1 py-5 pl-4 ">
+      <Card className="gap-5 p-5">
         <Typography variant="h4" className="text-primary">
         {AI_NAME}にようこそ！
         </Typography>
         <br/>
         <div className="flex flex-col gap-2">
-          <p className="">
+          <p style={p_style}>
           富士紡ChatGPTは文章の要約や生成、WEB検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。<br/>
           </p>
-          <p>この度、DXの一環として、2024年3月からプレビュー版を社内利用限定で公開いたします。</p>
+          <p style={p_style}>この度、DXの一環として、2024年3月からプレビュー版を社内利用限定で公開いたします。</p>
           <p style={p_style}>
             以下の注意事項をご確認の上、富士紡ChatGPTのご利用をお願いいたします。詳細は
             <b>
@@ -31,9 +31,9 @@ export const StartNewChat: FC<Prop> = (props) => {
             をご参照下さい。
             <br/>
           </p>
-          <p className="text-red-500">※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p>
+          <p className="text-red-500" style={p_style}>※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p>
           <br/>
-          <p>【注意事項】<br/>
+          <p style={p_style}>【注意事項】<br/>
           ・第三者の著作物を入力しない。固有名詞(既存著作物、作家名、作品の名称)を入力しない<br/>
           ・生成された回答結果に著作権侵害（既存の著作物との類似性・依拠性）がないか確認する<br/>
           ・回答結果の出典(根拠・裏付け)の確認や、回答結果の正確性を確認する<br/>
