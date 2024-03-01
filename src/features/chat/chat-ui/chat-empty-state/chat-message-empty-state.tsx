d7fb6c5094d4f3a2eed0809e0d7f92742172a5e7
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-01 16:22:45 +0900

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -58,7 +58,7 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
         <div className="flex justify-center flex-wrap gap-1 p-2">
           <div className="flex-none w-7 h-10"><AppWindow /></div>
           <div className="flex-none w-auto h-10 pr-4">
-            画面の説明は
+            本画面の説明は
             <b>
               <a href="http://groupeip1.intrafujibo.co.jp/pdf/富士紡ChatGPT利用マニュアル.pdf" target="_blank" className="text-blue-600 hover:underline">
                 こちら
