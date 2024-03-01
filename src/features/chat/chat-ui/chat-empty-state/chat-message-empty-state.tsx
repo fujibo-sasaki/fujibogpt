95409bbc69ed6a1defc4a5297ca5356a3369dd0e
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-01 17:27:02 +0900

    Update chat-message-empty-state.tsx

--- src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
+++ src/features/chat/chat-ui/chat-empty-state/chat-message-empty-state.tsx
@@ -49,7 +49,7 @@ export const ChatMessageEmptyState: FC<Prop> = (props) => {
         <br/>
         </p>
         <p className="text-sm text-red-500">※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p>
-        <p className="text-red-500" style={p_style}>※富士紡ChatGPT以外の個人向け生成AIは、情報漏洩の危険性があるため、利用を控えてください<br/><br/></p>
+        <p className="text-red-500">※富士紡ChatGPT以外の個人向け生成AIは、情報漏洩の危険性があるため、利用を控えてください<br/><br/></p>
         <p className="text-sm">【注意事項】<br/>
           ・第三者の著作物を入力しない。固有名詞(既存著作物、作家名、作品の名称)を入力しない<br/>
           ・生成された回答結果に著作権侵害（既存の著作物との類似性・依拠性）がないか確認する<br/>
