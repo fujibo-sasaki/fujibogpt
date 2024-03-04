efd454d563ad922088ebdc0414ed7787d3b7df24
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-04 09:46:33 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -17,7 +17,7 @@ export const StartNewChat: FC<Prop> = (props) => {
 
   const p_style = {fontSize: "15px"};
   return (
-    <div className="grid grid-cols-1 w-full items-center container mx-auto max-w-4xl justify-center h-full gap-9">
+    <div className="grid grid-cols-1 w-full items-center container mx-auto max-w-4xl justify-center h-full gap-9 overflow-scroll">
       <Card className="gap-5 p-5">
         <Typography variant="h4" className="text-primary">
         {AI_NAME}にようこそ！
