893af26da01f5ff211ea79eb566f1cf187d27e02
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-04 11:58:56 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -53,7 +53,7 @@ export const StartNewChat: FC<Prop> = (props) => {
            </div>
           </div>
         </div>
-        <div className="-mx-1 -mb-1 p-1 flex flex-col">
+        <div className="flex flex-col gap-1">
           <p>下記の➕を押して会話を開始できます</p>
         </div>
         <div className="-mx-1 -mb-1 p-1 flex flex-col border-t bg-muted">
