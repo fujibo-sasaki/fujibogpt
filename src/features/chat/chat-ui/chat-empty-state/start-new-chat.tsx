7e6d7ee4cfbf1160b14348c81bc1fd95b47859c7
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-04 11:19:39 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -42,8 +42,8 @@ export const StartNewChat: FC<Prop> = (props) => {
           ・業務以外のプライベートな利用は控える
           </p>
           <div className="flex justify-start flex-wrap gap-1 p-2">
-           <div className="flex-none w-7 h-10 gap-0"><AppWindow /></div>
-           <div className="flex-none w-auto h-10 min-w-3">
+           <div className="w-7 h-10 gap-0"><AppWindow /></div>
+           <div className="w-auto h-10 min-w-3">
             本画面の説明は
             <b>
              <a href="http://groupeip1.intrafujibo.co.jp/pdf/富士紡ChatGPT利用マニュアル.pdf" target="_blank" className="text-blue-600 hover:underline">
