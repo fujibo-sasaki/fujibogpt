93844d1d47ca81568e4bb84f5270390354f09901
fujibo-sasaki <159222958+fujibo-sasaki@users.noreply.github.com>
2024-03-14 16:38:43 +0900

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -25,15 +25,15 @@ export const StartNewChat: FC<Prop> = (props) => {
         <br/>
         <div className="flex flex-col gap-2">
           <p style={p_style}>
-          富士紡ChatGPTは文章の要約や生成、WEB検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。<br/>
+          富士紡ChatGPTは文章の要約や生成、Web検索が可能な富士紡グループ独自の生成AI(ChatGPT)です。<br/>
           </p>
-          <p style={p_style}>この度、DXの一環として、2024年3月からプレビュー版を社内利用限定で公開いたします。</p>
           <p style={p_style}>
             以下の注意事項をご確認の上、富士紡ChatGPTのご利用をお願いいたします。
             <br/>
           </p>
-          <p className="text-red-500" style={p_style}>※富士紡ChatGPTで質問した内容は、外部に漏れないようにセキュリティ対策を行っております</p>
+          <p className="text-red-500" style={p_style}>※質問情報は社外に漏洩することがないセキュアな環境となります</p>
           <p className="text-red-500" style={p_style}>※富士紡ChatGPT以外の個人向け生成AIは、情報漏洩の危険性があるため、利用を控えてください</p>
+          <p className="text-red-500" style={p_style}>※富士紡ChatGPT(Ver3.5)は画像生成に非対応です。画像生成が可能なVer4.0については今後検討します</p>
           <br/>
           <p style={p_style}>【注意事項】<br/>
           ・第三者の著作物を入力しない。固有名詞(既存著作物、作家名、作品の名称)を入力しない<br/>
